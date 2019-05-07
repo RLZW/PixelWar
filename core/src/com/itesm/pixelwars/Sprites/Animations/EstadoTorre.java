21372c754e338fd266e3616b1cc0bafb897b8c7e
Marlon97 <A01379404@itesm.mx>
2019-05-07 08:56:57 -0500

    Animación torre
    Creación de ENUM para daño y creación de animaciones para las torres

---
+++ core/src/com/itesm/pixelwars/Sprites/Animations/EstadoTorre.java
@@ -0,0 +1,5 @@
+package com.itesm.pixelwars.Sprites.Animations;
+
+public enum EstadoTorre {
+    SINDAÑO, DAÑO1, DAÑO2, DAÑO3;
+}
--- core/src/com/itesm/pixelwars/Sprites/Animations/TowerAnimation.java
+++ core/src/com/itesm/pixelwars/Sprites/Animations/TowerAnimation.java
@@ -9,20 +9,54 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class TowerAnimation {
 
+    private final Animation animacionDaño1;
+    private final Animation animacionDaño2;
+    private final Animation animacionDaño3;
     private Animation animacion;
     private Sprite sprite;
     private float timerAnimacion;
+    private EstadoTorre estadoTorre;
+    private TextureRegion[][] texturaTorre;
+    private TextureRegion[][] texturaTorreDaño1;
+    private TextureRegion[][] texturaTorreDaño2;
+    private TextureRegion[][] texturaTorreDaño3;
 
-    public TowerAnimation(float x, float y, Texture torre){
+    public TowerAnimation(float x, float y, Texture torre, Texture torreDaño1, Texture torreDaño2, Texture torreDaño3){
         // Cargar textura
         //Texture textura = new Texture("torreAzul");  // 400x66 = tamaño de la imagen
 
         // Crea una region
         TextureRegion region = new TextureRegion(torre);
         // Divide la región en frames de 32x64
-        TextureRegion[][] texturaTorre = region.split(69,124);
+        texturaTorre = region.split(69,124);
         animacion = new Animation(0.15f,texturaTorre[0][5],texturaTorre[0][4],texturaTorre[0][3],texturaTorre[0][2],texturaTorre[0][1],texturaTorre[0][0]);
         animacion.setPlayMode(Animation.PlayMode.LOOP);
+
+
+        //Primer Daño
+        TextureRegion regionDaño1 = new TextureRegion(torreDaño1);
+        // Divide la región en frames de 32x64
+        texturaTorreDaño1 = region.split(69,124);
+        animacionDaño1 = new Animation(0.15f,texturaTorreDaño1[0][5],texturaTorreDaño1[0][4],texturaTorreDaño1[0][3],texturaTorreDaño1[0][2],texturaTorreDaño1[0][1]);
+        animacionDaño1.setPlayMode(Animation.PlayMode.LOOP);
+
+        //Segundo Daño
+        TextureRegion regionDaño2 = new TextureRegion(torreDaño2);
+        // Divide la región en frames de 32x64
+        texturaTorreDaño2 = region.split(69,124);
+        animacionDaño2 = new Animation(0.15f,texturaTorreDaño2[0][1]);
+        animacionDaño2.setPlayMode(Animation.PlayMode.LOOP);
+
+        //Segundo Tercer Daño
+        TextureRegion regionDaño3 = new TextureRegion(torreDaño3);
+        // Divide la región en frames de 32x64
+        texturaTorreDaño3 = region.split(69,124);
+        animacionDaño3 = new Animation(0.15f,texturaTorreDaño3[0][1]);
+        animacionDaño3.setPlayMode(Animation.PlayMode.LOOP);
+
+
+
+        estadoTorre = EstadoTorre.SINDAÑO;
         timerAnimacion = 0;
         // Quieto
         sprite = new Sprite(texturaTorre[0][0]);
@@ -31,8 +65,28 @@ public class TowerAnimation {
 
     public void render(SpriteBatch batch){
         timerAnimacion += Gdx.graphics.getDeltaTime();
-        TextureRegion region = (TextureRegion) animacion.getKeyFrame(timerAnimacion);
-        batch.draw(region, sprite.getX(), sprite.getY());
+        switch (estadoTorre){
+            case SINDAÑO:
+                sprite.setRegion(texturaTorre[0][0]);
+                TextureRegion region = (TextureRegion) animacion.getKeyFrame(timerAnimacion);
+                batch.draw(region, sprite.getX(), sprite.getY());
+                break;
+            case DAÑO1:
+                sprite.setRegion(texturaTorreDaño1[0][1]);
+                TextureRegion regionDaño1 = (TextureRegion) animacionDaño1.getKeyFrame(timerAnimacion);
+                batch.draw(regionDaño1, sprite.getX(), sprite.getY());
+                break;
+            case DAÑO2:
+                sprite.setRegion(texturaTorreDaño2[0][1]);
+                TextureRegion regionDaño2 = (TextureRegion) animacionDaño2.getKeyFrame(timerAnimacion);
+                batch.draw(regionDaño2, sprite.getX(), sprite.getY());
+                break;
+            case DAÑO3:
+                sprite.setRegion(texturaTorreDaño3[0][1]);
+                TextureRegion regionDaño3 = (TextureRegion) animacionDaño3.getKeyFrame(timerAnimacion);
+                batch.draw(regionDaño3, sprite.getX(), sprite.getY());
+                break;
+        }
     }
 
 
