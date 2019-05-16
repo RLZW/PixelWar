84f39fbf1394b76d3269784652070ba0f61907a9
RLZW <rulazw@gmail.com>
2019-05-16 15:28:41 -0500

    Cambios

--- core/src/com/itesm/pixelwars/Sprites/Animations/minero.java
+++ core/src/com/itesm/pixelwars/Sprites/Animations/minero.java
@@ -3,12 +3,12 @@ package com.itesm.pixelwars.Sprites.Animations;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 
-public class Minero extends AnimacionGuerrero {
+public class minero extends AnimacionGuerrero {
 
     private int ataque;
     private static final float TIEMPO_BASE = 0.5f;
     private float tiempoAtaque = 0;
-    public Minero(float x, float y, Texture guerreroCaminando, Texture guerreroParado, Texture guerreroAtacando, int caminandoWidth, int caminandoHeight, int paradoWidth, int paradoHeight, int atacandoWidth, int atacandoHeight, int hp, int ataque, boolean equipoAzul, char unidad) {
+    public minero(float x, float y, Texture guerreroCaminando, Texture guerreroParado, Texture guerreroAtacando, int caminandoWidth, int caminandoHeight, int paradoWidth, int paradoHeight, int atacandoWidth, int atacandoHeight, int hp, int ataque, boolean equipoAzul, char unidad) {
         super(x, y, guerreroCaminando, guerreroParado, guerreroAtacando, caminandoWidth, caminandoHeight, paradoWidth, paradoHeight, atacandoWidth, atacandoHeight, hp, ataque, equipoAzul, unidad);
         this.ataque=ataque;
     }
